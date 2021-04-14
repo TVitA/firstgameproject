d1588cf378f7694ad5091194df86623dea8ec6bd
TVitA <toukakirishima20012019@gmail.com>
2021-04-14 18:51:02 +0300

    add class UnityEngine.Graphics.Sprite.cs

--- FirstGameProject/Libraries/UnityEngine/Graphics/Sprite.cs
+++ FirstGameProject/Libraries/UnityEngine/Graphics/Sprite.cs
@@ -1,19 +1,168 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Drawing;
+using OpenTK;
 using UnityEngine.Utilities;
 
 namespace UnityEngine.Graphics
 {
     public class Sprite : Object, IDisposable
     {
-        public Sprite(String filename)
+        private Boolean isDisposed;
+
+        private Texture2D texture;
+        private Color color;
+        private Vector2 scale;
+        private Int32 width;
+        private Int32 height;
+
+        private Single zOrder;
+        private Single rotation;
+        private Single textureRotation;
+
+        private Boolean flipX;
+        private Boolean flipY;
+
+        internal Sprite(Texture2D texture)
             : base()
         {
+            this.flipX = false;
+            this.flipY = false;
 
+            this.isDisposed = false;
+
+            this.texture = texture;
+            this.color = Color.White;
+            this.scale = Vector2.One;
+            this.width = texture.Width;
+            this.height = texture.Height;
+        }
+
+        internal Sprite(Texture2D texture, Rectangle rect)
+            : this(texture)
+        {
+            this.width = rect.Width;
+            this.height = rect.Height;
+        }
+
+        public Sprite(String filename)
+            : this(ResourseLoader.LoadTexture2D(filename))
+        {
+        }
+
+        public Sprite(String filename, Rectangle rect)
+            : this(ResourseLoader.LoadTexture2D(filename), rect)
+        {
+        }
+
+        internal Texture2D Texture2D
+        {
+            get => texture;
+
+            set => texture = value;
+        }
+
+        public Color Color
+        {
+            get => color;
+
+            set => color = value;
+        }
+
+        public Vector2 Scale
+        {
+            get => scale;
+
+            set => scale = value;
+        }
+
+        public Int32 Width
+        {
+            get => width;
+
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Parameter 'width' can't be less than 1!");
+                }
+
+                width = value;
+            }
+        }
+
+        public Int32 Height
+        {
+            get => height;
+
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Parameter 'height' can't be less than 1!");
+                }
+
+                height = value;
+            }
+        }
+
+        public Single ZOrder
+        {
+            get => zOrder;
+
+            set => zOrder = value;
+        }
+
+        public Single Rotation
+        {
+            get => rotation;
+
+            set => rotation = value;
+        }
+
+        public Single TextureRotation
+        {
+            get => textureRotation;
+
+            set => textureRotation = value;
+        }
+
+        public Boolean FlipX
+        {
+            get => flipX;
+
+            set => flipX = value;
+        }
+
+        public Boolean FlipY
+        {
+            get => flipY;
+
+            set => flipY = value;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose(Boolean disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    texture.Dispose();
+                }
+
+                isDisposed = true;
+            }
+        }
+
+        ~Sprite()
+        {
+            Dispose(false);
         }
     }
 }
